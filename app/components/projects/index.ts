87bdfc2feabdd1fc8703444ca2312dbb30bc514f
DavidDavila <daviddavilapino@gmail.com>
2016-10-30 12:33:17 +0100

    a little of styles

--- app/components/projects/index.ts
+++ app/components/projects/index.ts
@@ -25,12 +25,14 @@ export class Projects {
 		var infoBubble;
 
 		// Initial position of the map
-		var center = new google.maps.LatLng(-25.363882, 131.044922);
-
+		var center = new google.maps.LatLng(40.4167754, -3.7037901999999576);
+		let style= [{"featureType":"all","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"all","elementType":"geometry","stylers":[{"color":"#d6a41d"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"gamma":0.01},{"lightness":20}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"weight":2},{"gamma":0.8}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"on"},{"color":"#d6a41d"}]},{"featureType":"landscape","elementType":"geometry","stylers":[]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"saturation":20}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"lightness":20},{"saturation":-20}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":10},{"saturation":-30}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"saturation":25},{"lightness":25}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"lightness":-20},{"visibility":"on"},{"color":"#ffffff"}]}];
 	    // Map options
 	    var mapOptions = {
-	        zoom: 4,
+	        zoom: 3,
+	        scrollwheel: false,
 	        center: center,
+	        styles: style,
 	        mapTypeId: google.maps.MapTypeId.ROADMAP
 	    };
 	    // Attach the map to the document
@@ -38,12 +40,12 @@ export class Projects {
 	    mapOptions);
 	    // The marker
 	    var marker = new google.maps.Marker({
-	        position: new google.maps.LatLng(-25.363882, 131.044922),
+	        position: new google.maps.LatLng(40.4167754,  -3.7037901999999576),
 	        map: map,
 	        title: 'marker7'
 	    });
 
-
+	    // http://jsfiddle.net/landwire/WppF6/24/
 		
 	}
 
